4b4df3c4cc6eecfe1bb0972791c6f5b5638d0494
Jefferson Cargua <jeffersoncargua@gmail.com>
2026-01-13 22:35:30 -0500

    Se coloco una api fake para los datos

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,10 @@
 export const useFetch = async () => {
-	// var result = await fetch("/src/data/projects.json")
-	var result = await fetch("/public/data/projects.json")
+	// var result = await fetch("/src/data/projects.json") // https://69670e00bbe157c088b0be54.mockapi.io/proyectos/projects
+	var result = await fetch("https://69670e00bbe157c088b0be54.mockapi.io/proyectos/projects")
 		.then((response) => response)
 		.then((result) => result.json())
 		.catch((_error) => null);
 	return result;
 };
+
+// Se coloco la api fake en: https://mockapi.io/projects/69670e00bbe157c088b0be55
