e8080f1d8afb6e715b4ea88945cf4816f4c32420
Jefferson Cargua <jeffersoncargua@gmail.com>
2026-01-13 21:51:08 -0500

    Se cambio el data json a la carpeta public

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,6 @@
 export const useFetch = async () => {
-	var result = await fetch("/src/data/projects.json")
+	// var result = await fetch("/src/data/projects.json")
+	var result = await fetch("/public/data/projects.json")
 		.then((response) => response)
 		.then((result) => result.json())
 		.catch((_error) => null);
